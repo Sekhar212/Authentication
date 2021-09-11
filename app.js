bf46a411143d8fa1a266d14920a7a429b54c8ed0
Sekhar Babu <15691a03g0@gmail.com>
2021-09-11 19:05:41 +0530

    Included Hashin

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -19,8 +20,8 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-var secret = process.env.SECRET;
-userSchema.plugin(encrypt, {secret:secret, encryptedFields: ['password']});
+// var secret = process.env.SECRET;
+// userSchema.plugin(encrypt, {secret:secret, encryptedFields: ['password']});
 
 const User = mongoose.model("user", userSchema);
 
@@ -39,7 +40,7 @@ app.get("/login", (req, res) => {
 
 app.post("/register", (req, res) => {
   const email = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: email}, (err, foundOne) =>{
     if(!err){
@@ -69,7 +70,7 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
   const email = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: email}, (err, foundOne) =>{
     if(!err){
