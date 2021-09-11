62fa5a34192b93dadc075e3be86db3b95261778c
Sekhar Babu <15691a03g0@gmail.com>
2021-09-11 21:41:04 +0530

    Hashing and salting

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 // const encrypt = require('mongoose-encryption');
-const md5 = require('md5');
+// const md5 = require('md5');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 const app = express();
 
@@ -39,28 +42,36 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const email = req.body.username;
-  const password = md5(req.body.password);
 
-  User.findOne({email: email}, (err, foundOne) =>{
+   bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
     if(!err){
-      if(foundOne){
-        res.redirect("/register");
-      }
-      else{
-        const user = new User({
-          email: email,
-          password: password
-        });
+      const email = req.body.username;
+      const password = hash;
 
-        user.save((err, respo) => {
-          if(!err){
-            res.render('secrets');
-          }
-        });
-      }
+       User.findOne({email: email}, (err, foundOne) =>{
+         if(!err){
+           if(foundOne){
+             res.redirect("/register");
+           }
+           else{
+             const user = new User({
+               email: email,
+               password: password
+             });
+
+             user.save((err) => {
+               if(!err){
+                 res.render('secrets');
+               }
+             });
+           }
+         }
+         });
+
     }
-    });
+});
+
+
 
 
 
@@ -70,15 +81,18 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
   const email = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email: email}, (err, foundOne) =>{
     if(!err){
       if(foundOne){
         const dbPassword = foundOne.password;
-        if(dbPassword == password){
-          res.render('secrets');
-        }
+        bcrypt.compare(password, dbPassword, function(err, result) {
+          if(result == true && !err){
+            res.render('secrets');
+          }
+        });
+
       }
     }
   });
